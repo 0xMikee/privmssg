d6ff9e5651def74a5dd666dd17825de48077e6a6
-Mike- <60368562+0xMikee@users.noreply.github.com>
2024-06-24 22:38:41 +0200

    edited healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -7,14 +7,13 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 	try {
 		const url = new URL("/", `http://${host}`);
-		// if we can connect to the database and make a simple query
-		// and make a HEAD request to ourselves, then we're good.
-		await Promise.all([
-			prisma.note.count(),
-			fetch(url.toString(), { method: "HEAD" }).then((r) => {
-				if (!r.ok) return Promise.reject(r);
-			}),
-		]);
+		await prisma.note.count();
+
+		await fetch(url.toString(), { method: "HEAD" }).then((r) => {
+			if (!r.ok) {
+				return Promise.reject(r);
+			}
+		});
 		return new Response("OK ;)");
 	} catch (error: unknown) {
 		console.log("healthcheck ❌", { error });
